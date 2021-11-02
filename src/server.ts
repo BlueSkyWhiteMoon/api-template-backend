e34d972064f63b989e36b5a761461f9433172436
Muhamed <mutaliev96@outlook.com>
2021-11-02 11:47:42 +0100

    new methods

--- src/server.ts
+++ src/server.ts
@@ -30,6 +30,56 @@ app.post('/api/bands', async (req, res) => {
     res.send(
       `${newBand.Bandname} added to the Database with the id ${insertedBand.insertedId}!`
     );
+    console.log(insertedBand);
+  }
+});
+
+app.patch('/api/bands/:bandname/members', async (req, res) => {
+  const bandname = req.params.bandname;
+  const newMembers = req.body.Members;
+  const updatedBand = await getBandCollection().updateOne(
+    { Bandname: bandname },
+    { $push: { Members: newMembers } }
+  );
+  if (updatedBand.modifiedCount !== 0) {
+    res
+      .status(200)
+      .send(`${newMembers.name} is added into the Members list of ${bandname}`);
+  } else {
+    res.status(304).send(`Members list of ${bandname} were not modified.`);
+  }
+});
+
+app.patch('/api/bands/:bandname/:membersDelete', async (request, response) => {
+  const bandname = request.params.bandname;
+  const membersDelete = request.params.membersDelete;
+  const deleteMembers = await getBandCollection().updateOne(
+    { Bandname: bandname },
+    { $pull: { Members: { name: membersDelete } } }
+  );
+  if (deleteMembers.modifiedCount !== 0) {
+    response
+      .status(200)
+      .send(
+        `${membersDelete} was removed from the members list of ${bandname}`
+      );
+  } else {
+    response.send(
+      `Could not remove ${membersDelete}, because it does not exist in the the band document.`
+    );
+  }
+});
+
+app.get('/api/bands/:genre', async (request, response) => {
+  const genre = request.params.genre;
+  const genreOfBands = await getBandCollection()
+    .find({ Genre: genre })
+    .toArray();
+
+  if (genreOfBands.length > 0) {
+    response.send(genreOfBands);
+  } else {
+    response.status(404).send(`No Bands with the genre ${genre}`);
   }
 });
 
